d1060a1cb8b92c32ec6e41f4ad71f5ae88ea0539
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-15 20:53:29 +0400

    Exam006

---
+++ Exam006_/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача с гирьками
+
+int a = 1;
+int b = 6;
+int c = 8;
+int d = 3;
+int e = 4;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Maximum = ");
+Console.WriteLine(max);
